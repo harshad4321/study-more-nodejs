9830b34e822981ffe6d0b73991f578f181e0a1d9
Harshad N <103301901+harshad4321@users.noreply.github.com>
2022-06-08 16:17:38 +0530

    express basic  no.2

--- index.js
+++ index.js
@@ -7,19 +7,33 @@ console .log("2*5=", loggers( ));
 console.log(myname)
 //-----------------------------------
 
-const path = require("path");
+// app.use(express.static("view")) //app-level middleware
 
-app.get("/",(req,res)=>{
-    res.sendFile(path.join(__dirname,"view","index.html"));
-})
 
-app.get("/sent",(req,res)=>{
-    res.sendFile (path.join(__dirname,"view","app.html"))
-})
+app.get("/profile",Token,validaction ,(req,res)=>{ 
+    console.log("user is logged")
+     res.send("<h1>success..</h1>");   
+   });
 
-app.get("*",(req,res)=>{
-    res.status(404).send("<h1>404<h1>");  //chaining ;
-})
+
+function Token(req,res,next){
+    console.log("created tocken");
+    setInterval(()=>{
+        const Token= "123"
+        req.token=Token
+         next();
+    },1000);
+
+}
+function validaction (req,res,next){
+    if(req.token){
+    console.log(" tocken  approved")
+     next(); }
+   }
+
+// app.get("*",(req,res)=>{
+//     res.status(404).send("<h1>404<h1>");  //chaining ;
+// })
 
 
 const PORT = process.env.PORT||3000;
