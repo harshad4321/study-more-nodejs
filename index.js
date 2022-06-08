da6e91a476d745c46ffe6b56a4021a31cb9c5060
Harshad N <103301901+harshad4321@users.noreply.github.com>
2022-06-08 15:01:57 +0530

    express basic  no.1

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
-const http =require ('http');
-const fs = require("fs");
-const path = require("path");
+const express =require("express");
+const app = express();
+
+//--------------------------------
 const { loggers,myname} = require('./utils');
-console .log("2*5=", loggers()); 
+console .log("2*5=", loggers( )); 
 console.log(myname)
+//-----------------------------------
+
+const path = require("path");
+
+app.get("/",(req,res)=>{
+    res.sendFile(path.join(__dirname,"view","index.html"));
+})
+
+app.get("/sent",(req,res)=>{
+    res.sendFile (path.join(__dirname,"view","app.html"))
+})
+
+app.get("*",(req,res)=>{
+    res.status(404).send("<h1>404<h1>");  //chaining ;
+})
 
 
- const server=http.createServer((req,res)=>{
-     if(req.url ==='/'){
-     fs.readFile(path.join(__dirname,"view","index.html"),"utf8",(err,data)=>{
-         if(err) throw err;
-         res.writeHead(200,{"content-type":"text/html"});  
-        res.end(data);
-     });
-    }
-    if(req.url ==='/app'){
-        fs.readFile(path.join(__dirname,"view","app.html"),"utf8",(err,data)=>{
-            if(err) throw err;
-            res.writeHead(200,{"content-type":"text/html"});  
-           res.end(data);
-        });
-       }
-});
-
-const PORT =process.env.PORT||3000;
-server.listen(PORT,()=>{
-    console.log(`server running on ${PORT}`)
+const PORT = process.env.PORT||3000;
+app.listen(PORT,()=>{
+console.log(`server running in ${PORT}`)
 })
